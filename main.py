0c333d1a0a1da5154df14123795c5478b5e52a09
Celso Teofilo da Silva <90398200+celsoteofilo@users.noreply.github.com>
2022-08-15 21:32:46 -0300

    Botao Play ok

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ commandos = CommandsMusic()
 
 janela = Tk()
 janela.title ('VIdeo Music ')
-janela.geometry('300x300')
+janela.geometry('500x300')
 
 
-botao = Button(janela, text= 'PLAY', ) # crinado o botao , A funcao comando busca a funcao criada
-botao.grid(column= 30, row=40)# aqui apliquei a mesma funcao no grid
+botao = Button(janela, text= 'PLAY')
+botao.grid(column= 30, row=40)
+
+botao = Button(janela, text= 'STOP')
+botao.grid(column= 30, row=80)
+
+
+botao = Button(janela, text= 'NEXT')
+botao.grid(column= 40, row=50)
+
+botao = Button(janela, text= ' COME BACK')
+botao.grid(column= 10, row=50)
+
+botao = Button(janela, text= 'SAIR')
+botao.grid(column= 30, row=90)
+
 
 
 Texto_play = Label(janela, text= 'PARA TOCAR: ' )
 Texto_play.grid(column=30, row=0)
-Texto_play.grid(padx= 30,pady=10)
+Texto_play.grid(padx= 30,pady=0)
 
 
 
